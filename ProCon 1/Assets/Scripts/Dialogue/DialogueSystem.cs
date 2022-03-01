7a0ec3be567ad62cd2e87db1a18aebc9f3c7b65e
Jelllybean <jellesimonvds@gmail.com>
2022-03-01 16:14:54 +0100

    beginning typewriter system

---
+++ ProCon 1/Assets/Scripts/Dialogue/DialogueSystem.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueSystem : MonoBehaviour
+{
+	[SerializeField] private TMP_Text _tmpProText;
+	string writer;
+
+	[SerializeField] float delayBeforeStart = 0f;
+	[SerializeField] float timeBtwChars = 0.1f;
+	[SerializeField] string leadingChar = "";
+	[SerializeField] bool leadingCharBeforeDelay = false;
+
+	void Start()
+	{
+		//_tmpProText = GetComponent<TMP_Text>()!;
+
+		if (_tmpProText != null)
+		{
+			writer = _tmpProText.text;
+			_tmpProText.text = "";
+
+			StartCoroutine("TypeWriterTMP");
+		}
+	}
+
+	IEnumerator TypeWriterTMP()
+	{
+		_tmpProText.text = leadingCharBeforeDelay ? leadingChar : "";
+
+		yield return new WaitForSeconds(delayBeforeStart);
+
+		foreach (char c in writer)
+		{
+			if (_tmpProText.text.Length > 0)
+			{
+				_tmpProText.text = _tmpProText.text.Substring(0, _tmpProText.text.Length - leadingChar.Length);
+			}
+			_tmpProText.text += c;
+			_tmpProText.text += leadingChar;
+			yield return new WaitForSeconds(timeBtwChars);
+		}
+		_tmpProText.text += "\npress space to continue";
+		if (leadingChar != "")
+		{
+			_tmpProText.text = _tmpProText.text.Substring(0, _tmpProText.text.Length - leadingChar.Length);
+		}
+	}
+}
