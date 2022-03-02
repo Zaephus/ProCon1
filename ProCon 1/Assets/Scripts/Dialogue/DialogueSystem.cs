764ac1b89a0bb88c5a14ae3f0e460b25e7b759ee
Jelllybean <jellesimonvds@gmail.com>
2022-03-02 14:56:06 +0100

    working on letting the player choose things

--- ProCon 1/Assets/Scripts/Dialogue/DialogueSystem.cs
+++ ProCon 1/Assets/Scripts/Dialogue/DialogueSystem.cs
@@ -7,6 +7,9 @@ using TMPro;
 public class DialogueSystem : MonoBehaviour
 {
 	[SerializeField] private TMP_Text _tmpProText;
+	[SerializeField] private List<string> dialogueList = new List<string>();
+	[SerializeField] private List<int> inputBreak = new List<int>(); 
+	[SerializeField] private GameObject dialogueBox;
 	string writer;
 
 	[SerializeField] float delayBeforeStart = 0f;
@@ -14,27 +17,63 @@ public class DialogueSystem : MonoBehaviour
 	[SerializeField] string leadingChar = "";
 	[SerializeField] bool leadingCharBeforeDelay = false;
 
+	private int index = -1;
+	private int inputIndex = 0;
+
 	void Start()
 	{
-		//_tmpProText = GetComponent<TMP_Text>()!;
-
 		if (_tmpProText != null)
 		{
 			writer = _tmpProText.text;
-			_tmpProText.text = "";
-
-			StartCoroutine("TypeWriterTMP");
 		}
 	}
 
-	IEnumerator TypeWriterTMP()
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			if(index == inputBreak[inputIndex])
+			{
+				_tmpProText.text = "doe even alsof hier nu knoppen staan oke";
+			}
+			else
+			{
+				index++;
+				Debug.Log("index: " + index + " lengte: " + dialogueList.Count);
+				if (index == dialogueList.Count)
+				{
+					Debug.Log("AAAAH" + index);
+					index = 0;
+					dialogueBox.SetActive(false);
+				}
+				else
+				{
+					StartDialogue();
+				}
+			}
+		}
+	}
+
+	public void StartDialogue()
+	{
+		dialogueBox.SetActive(true);
+		_tmpProText.text = "";
+		StartCoroutine(TypeWriterTMP(dialogueList[index]));
+	}
+
+	IEnumerator TypeWriterTMP(string _currentDialogue)
 	{
 		_tmpProText.text = leadingCharBeforeDelay ? leadingChar : "";
 
+		int _thisIndex = index;
 		yield return new WaitForSeconds(delayBeforeStart);
 
-		foreach (char c in writer)
+		foreach (char c in _currentDialogue)
 		{
+			if(index != _thisIndex)
+			{
+				yield break;
+			}
 			if (_tmpProText.text.Length > 0)
 			{
 				_tmpProText.text = _tmpProText.text.Substring(0, _tmpProText.text.Length - leadingChar.Length);
@@ -43,7 +82,10 @@ public class DialogueSystem : MonoBehaviour
 			_tmpProText.text += leadingChar;
 			yield return new WaitForSeconds(timeBtwChars);
 		}
+
+		yield return new WaitForSeconds(0.35f);
 		_tmpProText.text += "\npress space to continue";
+
 		if (leadingChar != "")
 		{
 			_tmpProText.text = _tmpProText.text.Substring(0, _tmpProText.text.Length - leadingChar.Length);
