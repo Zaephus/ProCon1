d5f2d1fa39a889fba0e2dd18000a11b1e0b3b3d7
Jelllybean <jellesimonvds@gmail.com>
2022-03-03 16:41:44 +0100

    Got percentages working

--- ProCon 1/Assets/Scripts/Dialogue/DialogueOptions.cs
+++ ProCon 1/Assets/Scripts/Dialogue/DialogueOptions.cs
@@ -7,6 +7,7 @@ public class DialogueOptions : ScriptableObject
 {
     public List<string> dialogueList = new List<string>();
     public List<string> responseOptions = new List<string>();
+    public List<int> chosenAmount = new List<int>(new int[4]);
     public int inputBreak;
     public List<DialogueOptions> nextDialogueOptions = new List<DialogueOptions>();
 
