193760b7b90202c19e9129d53b704df30a2148cd
Marc Rozendaal <rozendaalmarc@gmail.com>
2022-03-06 23:45:45 +0100

    NPC Interaction Improvement

--- ProCon 1/Assets/Scripts/PlayerInteract.cs
+++ ProCon 1/Assets/Scripts/PlayerInteract.cs
@@ -7,6 +7,8 @@ public class PlayerInteract : MonoBehaviour {
 
     public Image thingy;
 
+    private Vector2 promptPosition;
+
     public void Start() {
 
         thingy.enabled = false;
@@ -15,7 +17,7 @@ public class PlayerInteract : MonoBehaviour {
 
     public void OnTriggerEnter2D(Collider2D other) {
 
-        Vector2 promptPosition = other.transform.position;
+        promptPosition = other.transform.position;
         promptPosition.y += 1;
 
         thingy.enabled = true;
@@ -25,12 +27,17 @@ public class PlayerInteract : MonoBehaviour {
 
     public void OnTriggerStay2D(Collider2D other) {
 
+        promptPosition = other.transform.position;
+        promptPosition.y += 1;
+
         if(other.GetComponent<IInteractable>() != null) {
             IInteractable interact = other.GetComponent<IInteractable>();
             if(Input.GetButtonDown("Interact")) {
                 interact.Interact();
             }
         }
+
+        thingy.transform.position = promptPosition;
 
     }
 
