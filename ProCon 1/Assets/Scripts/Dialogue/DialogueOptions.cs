bc6ad3af7a596fe52b6798ae5e33b8427b63fc4a
Marc Rozendaal <rozendaalmarc@gmail.com>
2022-03-23 03:58:40 +0100

    Dialoog fix, sprites in battle, en laatste level updates

--- ProCon 1/Assets/Scripts/Dialogue/DialogueOptions.cs
+++ ProCon 1/Assets/Scripts/Dialogue/DialogueOptions.cs
@@ -7,9 +7,10 @@ public class DialogueOptions : ScriptableObject
 {
     public List<string> dialogueList = new List<string>();
     public List<string> responseOptions = new List<string>();
-    public List<int> chosenAmount = new List<int>(new int[4]);
+    public List<int> chosenAmount = new List<int>();
     public int inputBreak;
     public int combatBreak = -1;
+    public int endBreak = -1;
     public List<DialogueOptions> nextDialogueOptions = new List<DialogueOptions>();
 
 }
